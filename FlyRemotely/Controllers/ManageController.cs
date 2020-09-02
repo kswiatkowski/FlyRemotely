32e4af3e5f8788e086b74e0980fe3777492fe585
kswiatkowski <kaswiatkowski@gmail.com>
2020-09-02 09:30:00 +0200

    Add password change

---
+++ FlyRemotely/Controllers/ManageController.cs
@@ -0,0 +1,138 @@
+﻿using FlyRemotely.App_Start;
+using FlyRemotely.Models;
+using FlyRemotely.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyRemotely.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            Error
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        // GET: Manage
+        public async Task<ActionResult> Index(ManageMessageId? message)
+        {
+            if (TempData["ViewData"] != null)
+            {
+                ViewData = (ViewDataDictionary)TempData["ViewData"];
+            }
+
+            if (User.IsInRole("Admin"))
+                ViewBag.UserIsAdmin = true;
+            else
+                ViewBag.UserIsAdmin = false;
+
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var model = new ManageCredentialsViewModel
+            {
+                Message = message
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangeProfile()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                var result = await UserManager.UpdateAsync(user);
+
+                AddErrors(result);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword([Bind(Prefix = "ChangePasswordViewModel")] ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInAsync(user, isPersistent: false);
+                }
+                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+            }
+            AddErrors(result);
+
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            var message = ManageMessageId.ChangePasswordSuccess;
+            return RedirectToAction("Index", new { Message = message });
+        }
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("password-error", error);
+            }
+        }
+
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie, DefaultAuthenticationTypes.TwoFactorCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = isPersistent }, await user.GenerateUserIdentityAsync(UserManager));
+        }
+    }
+}
---
+++ FlyRemotely/ViewModels/ManageViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlyRemotely.ViewModels
+{
+    public class ManageCredentialsViewModel
+    {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public FlyRemotely.Controllers.ManageController.ManageMessageId? Message { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktualne hasło")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Zbyt mała liczba znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź nowe hasło")]
+        [Compare("NewPassword", ErrorMessage = "Hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
